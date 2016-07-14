e028c7ca987a9a0f3f60226040cf1273c3737e3d
thuanth <thuanth6589@seta-asia.com.vn>
2016-07-14 15:27:07 +0700

    - navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,36 +35,51 @@
                     <span class="icon-bar"></span>
                 </button>
 
-                <!-- Branding Image
-                <a class="navbar-brand" href="{{ url('/') }}">
+
+                <a class="navbar-brand" href="">
                     USAMI
                 </a>
-                -->
+
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">USAMI</a></li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown" class="dropdown-toggle" href="#">City <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">List</a></li>
+                            <li><a href="#">Create</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown" class="dropdown-toggle" href="#">District <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">List</a></li>
+                            <li><a href="#">Create</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown" class="dropdown-toggle" href="#">Customer <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{route('list_customers')}}">List</a></li>
+                            <li><a href="#">Create</a></li>
+                        </ul>
+                    </li>
                 </ul>
-
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links
-                    @if (Auth::guest())
-                       <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
+                    @if ($user = Session::get('user'))
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ $user['user'] }} <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
-                    @endif -->
+                    @endif
                 </ul>
             </div>
         </div>
