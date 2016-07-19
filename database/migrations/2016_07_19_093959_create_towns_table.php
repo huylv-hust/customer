368d5b78d517ee60037d2ba205de2fc107f56b99
thuanth <thuanth6589@seta-asia.com.vn>
2016-07-19 09:46:32 +0700

    - table towns

---
+++ database/migrations/2016_07_19_093959_create_towns_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTownsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('towns', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('city_id');
+            $table->integer('district_id');
+            $table->string('name', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('towns');
+    }
+}
